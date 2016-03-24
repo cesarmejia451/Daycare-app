a28c83af79705322771c66e780d0671e517b2d36
Cesar <cesarmejia451@gmail.com>
2016-03-23 20:12:22 -0500

    fixed some minor CSS grid layouts on the show page.

--- app/models/center.rb
+++ app/models/center.rb
@@ -42,8 +42,11 @@ class Center < ActiveRecord::Base
   end 
 
   def random_image
+
     image = ["https://bfb89515afffff903eb7a381cf5e58e24a620c1b-www.googledrive.com/host/0B09DNIgcGom_b1hxS2JnYXBiWFk/daycareimg.jpg", "https://bfb89515afffff903eb7a381cf5e58e24a620c1b-www.googledrive.com/host/0B09DNIgcGom_b1hxS2JnYXBiWFk/daycare_toy.jpg", "https://bfb89515afffff903eb7a381cf5e58e24a620c1b-www.googledrive.com/host/0B09DNIgcGom_b1hxS2JnYXBiWFk/Logo2.jpg", "https://bfb89515afffff903eb7a381cf5e58e24a620c1b-www.googledrive.com/host/0B09DNIgcGom_b1hxS2JnYXBiWFk/toy_1.jpg", "https://bfb89515afffff903eb7a381cf5e58e24a620c1b-www.googledrive.com/host/0B09DNIgcGom_b1hxS2JnYXBiWFk/toy_org.jpg", "https://bfb89515afffff903eb7a381cf5e58e24a620c1b-www.googledrive.com/host/0B09DNIgcGom_b1hxS2JnYXBiWFk/toy_duck.jpg"]
-    image.random 
+    url = image.shuffle
+    p url.first
+
   end
 
 end
